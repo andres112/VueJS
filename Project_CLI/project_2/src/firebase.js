538d63c16fda8240ad43866c148bee44896a6439
Andres Dorado <andres.dorado90@gmail.com>
2020-07-17 20:46:02 +0200

    firestore.js modified: include authentication librery and const

--- Project_CLI/project_2/src/firebase.js
+++ Project_CLI/project_2/src/firebase.js
@@ -1,20 +1,21 @@
-import firebase from 'firebase/app' // Import firebase 
-import 'firebase/firestore' // the tables in firebase are called firestore
+import firebase from "firebase/app"; // Import firebase
+import "firebase/firestore"; // the tables in firebase are called firestore
+import "firebase/auth"; // Lib in charge of authentication in firebase
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBFKZWaEpKVtJgl-TlWmvsuM6nsvhARY2g",
-    authDomain: "pokemon-afdd.firebaseapp.com",
-    databaseURL: "https://pokemon-afdd.firebaseio.com",
-    projectId: "pokemon-afdd",
-    storageBucket: "pokemon-afdd.appspot.com",
-    messagingSenderId: "308948632737",
-    appId: "1:308948632737:web:d6468b0cf1a99c51e6dce0"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyBFKZWaEpKVtJgl-TlWmvsuM6nsvhARY2g",
+  authDomain: "pokemon-afdd.firebaseapp.com",
+  databaseURL: "https://pokemon-afdd.firebaseio.com",
+  projectId: "pokemon-afdd",
+  storageBucket: "pokemon-afdd.appspot.com",
+  messagingSenderId: "308948632737",
+  appId: "1:308948632737:web:d6468b0cf1a99c51e6dce0",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const db = firebase.firestore();
+const db = firebase.firestore(); // const used to work with tables in firestore
+const auth = firebase.auth(); // const used to work with auth in firestore
 
-  export{db};
-
+export { db, auth };
