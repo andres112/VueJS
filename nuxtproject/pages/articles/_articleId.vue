b4481ef9e1e1221e0fc8e0bb84d23b5de2b53cf2
Andres Dorado <andres.dorado90@gmail.com>
2022-04-21 23:31:31 +0200

    nuxt routes and nuxt link

---
+++ nuxtproject/pages/articles/_articleId.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>Article {{ $route.params.articleId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticlePage'
+}
+</script>
---
+++ nuxtproject/pages/articles/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>Articles</h1>
+    <ul>
+      <li v-for="article in articles" :key="article.id" class="text-danger">
+        <NuxtLink :to="`/articles/${article.id}`">
+          {{ article.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ArticlesPage',
+  asyncData () {
+    return {
+      articles: [
+        { id: 1, title: 'Article 1' },
+        { id: 2, title: 'Article 2' },
+        { id: 3, title: 'Article 3' },
+        { id: 4, title: 'Article 4' },
+        { id: 5, title: 'Article 5' }
+      ]
+    }
+  }
+}
+</script>
