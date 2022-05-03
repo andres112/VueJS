5bbb8cbcbf90af549d90550648947058c2162a9e
Andres Dorado <andres.dorado90@gmail.com>
2022-05-03 22:12:02 +0200

    CRUD -U for tasks using axios and restAPI realtime database

--- nuxtproject/pages/todolist/index.vue
+++ nuxtproject/pages/todolist/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container-fluid dark-mode full-height">
     <h1>List of ToDo's</h1>
+    <b-card bg-variant="transparent">
+      <label for="input-task">Task:</label>
+      <b-input id="input-task" v-model="task" placeholder="Enter your task" />
+      <b-card-footer class="px-0">
+        <b-button variant="success" @click.prevent="addTask()">
+          Add
+        </b-button>
+      </b-card-footer>
+    </b-card>
     <b-card
       v-for="todo in todos"
       :key="todo.id"
@@ -11,7 +20,7 @@
         {{ todo }}
       </b-card-body>
       <b-card-footer>
-        <b-button variant="danger">
+        <b-button variant="danger" @click.prevent="deleteTask(todo.id)">
           Delete
         </b-button>
         <b-button variant="primary">
@@ -23,17 +32,30 @@
 </template>
 
 <script>
-// import { db, ref, onValue } from '@/plugins/firebase.js'
-import { computed, useStore } from '@nuxtjs/composition-api'
+import { computed, ref, useStore } from '@nuxtjs/composition-api'
 export default {
   name: 'TodoListPage',
   setup () {
     const store = useStore()
+    const task = ref('')
+
     const todos = computed(() => {
       return store.state.tasks
     })
+
+    const addTask = () => {
+      store.dispatch('addTask', { id: Date.now(), name: task.value })
+      task.value = ''
+    }
+
+    const deleteTask = (id) => {
+      store.dispatch('deleteTask', id)
+    }
     return {
-      todos
+      todos,
+      task,
+      addTask,
+      deleteTask
     }
   },
   fetch ({ store }) {
--- nuxtproject/store/index.js
+++ nuxtproject/store/index.js
@@ -7,6 +7,9 @@ export const state = () => ({
 export const mutations = {
   setTasks (state, tasks) {
     state.tasks = tasks
+  },
+  deleteTask (state, id) {
+    state.tasks = state.tasks.filter(task => task.id !== id)
   }
 }
 
@@ -16,7 +19,29 @@ export const actions = {
       const list = await axios.get(
         'https://todo-listo-c4f34-default-rtdb.firebaseio.com/tasks.json'
       )
-      commit('setTasks', list.data)
+      console.log(list.data)
+      commit('setTasks', Object.values(list.data))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async addTask ({ commit, state }, task) {
+    try {
+      await axios.put(
+        `https://todo-listo-c4f34-default-rtdb.firebaseio.com/tasks/${task.id}.json`,
+        task
+      )
+      commit('setTasks', [...state.tasks].concat(task))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async deleteTask ({ commit }, id) {
+    try {
+      await axios.delete(
+        `https://todo-listo-c4f34-default-rtdb.firebaseio.com/tasks/${id}.json`
+      )
+      commit('deleteTask', id)
     } catch (error) {
       console.error(error)
     }
