e15484f6e5145b2e7fc9235ca62b493c4ac5baf5
Andres Dorado <andres.dorado90@gmail.com>
2022-05-07 16:05:03 +0200

    add firebase config file

---
+++ viteproject/vite-pinia/src/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3hfBmnrkvLqC0BsYI3jS19sV4kE8QQWg",
+  authDomain: "todo-listo-c4f34.firebaseapp.com",
+  databaseURL: "https://todo-listo-c4f34-default-rtdb.firebaseio.com",
+  projectId: "todo-listo-c4f34",
+  storageBucket: "todo-listo-c4f34.appspot.com",
+  messagingSenderId: "849847176628",
+  appId: "1:849847176628:web:01da2c05809041e3ab76e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
